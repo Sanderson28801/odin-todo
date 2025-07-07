d4d07818592ea6a7172fc11ccd7b17fc856a4029
Spencer Anderson <sanderson28801@gmail.com>
2025-07-07 16:05:00 -0400

    started working on styling and basic page layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+/*
+Design Plan:
+3 files:
+    index.js- pulls it all together and renders everything on the dom
+        Caches dom
+    toDoFunctions: render function
+                        Many functions within to render everything
+
+                    Create todos/projects
+                    
+
+*/
+
